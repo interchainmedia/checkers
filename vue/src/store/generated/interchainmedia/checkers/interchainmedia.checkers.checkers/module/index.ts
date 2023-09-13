4c66da108641a31dc7aec4821f1ecea63aec6a13
Sykamiotis Aggelos <aggelossik@lizardhost.gr>
2023-09-13 20:00:35 +0300

    record the winner

--- vue/src/store/generated/interchainmedia/checkers/interchainmedia.checkers.checkers/module/index.ts
+++ vue/src/store/generated/interchainmedia/checkers/interchainmedia.checkers.checkers/module/index.ts
@@ -4,10 +4,12 @@ import { StdFee } from "@cosmjs/launchpad";
 import { SigningStargateClient } from "@cosmjs/stargate";
 import { Registry, OfflineSigner, EncodeObject, DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { Api } from "./rest";
+import { MsgPlayMove } from "./types/checkers/tx";
 import { MsgCreateGame } from "./types/checkers/tx";
 
 
 const types = [
+  ["/interchainmedia.checkers.checkers.MsgPlayMove", MsgPlayMove],
   ["/interchainmedia.checkers.checkers.MsgCreateGame", MsgCreateGame],
   
 ];
@@ -41,6 +43,7 @@ const txClient = async (wallet: OfflineSigner, { addr: addr }: TxClientOptions =
 
   return {
     signAndBroadcast: (msgs: EncodeObject[], { fee, memo }: SignAndBroadcastOptions = {fee: defaultFee, memo: ""}) => client.signAndBroadcast(address, msgs, fee,memo),
+    msgPlayMove: (data: MsgPlayMove): EncodeObject => ({ typeUrl: "/interchainmedia.checkers.checkers.MsgPlayMove", value: MsgPlayMove.fromPartial( data ) }),
     msgCreateGame: (data: MsgCreateGame): EncodeObject => ({ typeUrl: "/interchainmedia.checkers.checkers.MsgCreateGame", value: MsgCreateGame.fromPartial( data ) }),
     
   };
